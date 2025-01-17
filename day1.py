9421b27915b3ed049bea6de09a00ae13239b6d18
mayureshpawashe <mayureshpawashe3036@gmail.com>
2025-01-17 16:02:55 +0530

    completed day2

--- day1.py
+++ day1.py
@@ -1,111 +1,111 @@
-#Day1
-#Variables, Data Types, and Basic Operations
-# #type of variables
-# x=5
-# y=34.433
-# z=("mayur","hii")
-# a=["mayur","goodmorning"]
-# print(type(x))
-# print(type(y))
-# print(type(z))
-# print(type(a))
-# Output:
-# <class 'int'>
-# <class 'float'>
-# <class 'tuple'>
-# <class 'list'>
+Day1
+Variables, Data Types, and Basic Operations
+#type of variables
+x=5
+y=34.433
+z=("mayur","hii")
+a=["mayur","goodmorning"]
+print(type(x))
+print(type(y))
+print(type(z))
+print(type(a))
+Output:
+<class 'int'>
+<class 'float'>
+<class 'tuple'>
+<class 'list'>
 
-# Simple arithmetic operations
-# a = 10
-# b = 5
-#
-# print("Addition:", a + b)       # Output: 15
-# print("Subtraction:", a - b)    # Output: 5
-# print("Multiplication:", a * b) # Output: 50
-# print("Division:", a / b)       # Output: 2.0
+Simple arithmetic operations
+a = 10
+b = 5
 
-# String concatenation
-# a = "Hello"
-# name = str(input("Enter Your Name: "))
-# print(a + ", " + name + "!")  # Output: Hello, Alice!
-#
-# output:
-# Enter Your Name: Mayuresh
-# Hello, Mayuresh!
+print("Addition:", a + b)       # Output: 15
+print("Subtraction:", a - b)    # Output: 5
+print("Multiplication:", a * b) # Output: 50
+print("Division:", a / b)       # Output: 2.0
 
-#Dictionary to store personal details
-# personal_details = {
-#     "name": "Mayuresh",
-#     "age": 23,
-#     "city": "Shrirampur",
-#     "pincode":413709,
-#     "hobbies": ["reading", "cycling", "Travelling"]
-#                     }
+String concatenation
+a = "Hello"
+name = str(input("Enter Your Name: "))
+print(a + ", " + name + "!")  # Output: Hello, Alice!
 
-#Accessing my personal info
-# print("Name is",personal_details["name"])
-# print("Age is",personal_details["age"])
-#
-# Output:
-# Name is Mayuresh
-# Age is 23
+output:
+Enter Your Name: Mayuresh
+Hello, Mayuresh!
 
+Dictionary to store personal details
+personal_details = {
+    "name": "Mayuresh",
+    "age": 23,
+    "city": "Shrirampur",
+    "pincode":413709,
+    "hobbies": ["reading", "cycling", "Travelling"]
+                    }
 
-#Typecasting
+Accessing my personal info
+print("Name is",personal_details["name"])
+print("Age is",personal_details["age"])
 
-#String to Tuple
-# y='mayuresh'
-# x=tuple(y)
-# print(type(x))
-# print(x)
-#
-# Output:
-# <class 'tuple'>
-# ('m', 'a', 'y', 'u', 'r', 'e', 's', 'h')
+Output:
+Name is Mayuresh
+Age is 23
 
 
+Typecasting
 
-#String to Integer
-# str='200'
-# num=int(str)
-# print(type(str))
-# print(type(num))
-# print(num)
-#
-# Output:
-# <class 'str'>
-# <class 'int'>
-# 200
+String to Tuple
+y='mayuresh'
+x=tuple(y)
+print(type(x))
+print(x)
 
-
-#Int to Str
-# int=123
-# a=str(int)
-# print(a)
-# print(type(int))
-# print(type(a))
-#
-# Output:
-# 123
-# <class 'int'>
-# <class 'str'>
+Output:
+<class 'tuple'>
+('m', 'a', 'y', 'u', 'r', 'e', 's', 'h')
 
 
 
-#List to Tuple
-# l1=["Hii","iam","Mayuresh"]
-# t1=tuple(l1)
-# print(type(l1))
-# print(type(t1))
-# print(t1)
-#
-#
-# Output:
-# <class 'list'>
-# <class 'tuple'>
-# ('Hii', 'iam', 'Mayuresh')
+String to Integer
+str='200'
+num=int(str)
+print(type(str))
+print(type(num))
+print(num)
 
-#
+Output:
+<class 'str'>
+<class 'int'>
+200
+
+
+Int to Str
+int=123
+a=str(int)
+print(a)
+print(type(int))
+print(type(a))
+
+Output:
+123
+<class 'int'>
+<class 'str'>
+
+
+
+List to Tuple
+l1=["Hii","iam","Mayuresh"]
+t1=tuple(l1)
+print(type(l1))
+print(type(t1))
+print(t1)
+
+
+Output:
+<class 'list'>
+<class 'tuple'>
+('Hii', 'iam', 'Mayuresh')
+
+
 
 
 
--- day2.py
+++ day2.py
@@ -1,166 +1,166 @@
-# Day 2
-# Control Flow (If-Else Statements and Loops)
-#
-#
-# if-else
-# m=int(input("Enter Your Percentage::"))
-# if m > 75:
-#     print("First Class")
-# elif m <= 75 and m > 50:
-#     print("Second Class")
-# else:
-#     print("Failed")
-#
-#
-# Output:
-# Enter Your Percentage::76
-# First Class
-#
-# Enter Your Percentage::67
-# Second Class
-#
-# Enter Your Percentage::30
-# Failed
-#
-#
-# nested if
-# x=25
-# if x > 20:
-#     print("Greater than 20")
-#
-#     if x > 30:
-#         print("Greater than 30")
-#     else:
-#         print("Not Above 30")
-#
-#
-# Output:
-# Greater than 20
-# Not Above 30
-#
-#
-#
-#
-# While Loop
-# i=1
-# while i < 6:
-#     print(i)
-#     i=i+1
-#
-# Output:
-# 1
-# 2
-# 3
-# 4
-# 5
-#
-#
-#
-# sum of numbers to limit
-# total = 0
-# i=1
-# n=int(input("Enter The Limit: "))
-# while i <= n:
-#     total= total + i
-#     i=i+1
-# print(total)
-#
-# Output:
-# Enter The Limit: 3
-# 6
-#
-#
-# Factorial of limit
-# num=int(input("enter limit: "))
-# fact=1
-# i=1
-# while i<= num :
-#     fact=fact*i
-#     i=i+1
-# print("Fact is",fact)
-#
-# Output:
-# enter limit: 5
-# Fact is 120
-#
-#
-# For Loop
-# fruits = ["apple", "banana", "cherry"]
-# for fruit in fruits:
-#     print(fruit)
-#
-# Output:
-# apple
-# banana
-# cherry
+Day 2
+Control Flow (If-Else Statements and Loops)
+
+
+if-else
+m=int(input("Enter Your Percentage::"))
+if m > 75:
+    print("First Class")
+elif m <= 75 and m > 50:
+    print("Second Class")
+else:
+    print("Failed")
+
+
+Output:
+Enter Your Percentage::76
+First Class
+
+Enter Your Percentage::67
+Second Class
+
+Enter Your Percentage::30
+Failed
+
+
+nested if
+x=25
+if x > 20:
+    print("Greater than 20")
+
+    if x > 30:
+        print("Greater than 30")
+    else:
+        print("Not Above 30")
+
+
+Output:
+Greater than 20
+Not Above 30
 
 
 
-# student = {"name": "Mayuresh", "age": 23, "marks": 85}
-#
-# for key, value in student.items():
-#     print(key+":",value)
-#
-# Output:
-# name: Mayuresh
-# age: 23
-# marks: 85
 
+While Loop
+i=1
+while i < 6:
+    print(i)
+    i=i+1
 
-# rows = 5
-# for i in range(1, rows + 1):
-#     for j in range(i):
-#         print("*", end="")
-#     print()
-#
-# Output:
-# *
-# **
-# ***
-# ****
-# *****
+Output:
+1
+2
+3
+4
+5
 
 
 
-#Break Statement
-# for num in range(1,10):
-#     if num == 5:
-#         break  # breaking the current loop
-#     print(num)
-#
-# Output:
-# 1
-# 2
-# 3
-# 4
+sum of numbers to limit
+total = 0
+i=1
+n=int(input("Enter The Limit: "))
+while i <= n:
+    total= total + i
+    i=i+1
+print(total)
+
+Output:
+Enter The Limit: 3
+6
+
+
+Factorial of limit
+num=int(input("enter limit: "))
+fact=1
+i=1
+while i<= num :
+    fact=fact*i
+    i=i+1
+print("Fact is",fact)
+
+Output:
+enter limit: 5
+Fact is 120
+
+
+For Loop
+fruits = ["apple", "banana", "cherry"]
+for fruit in fruits:
+    print(fruit)
+
+Output:
+apple
+banana
+cherry
 
 
 
-#continue statement
-# for num in range(1,10):
-#     if num%2 == 0:
-#         continue    #here continue statement skip the even numbers
-#     print(num)
-#
-# Output:
-# 1
-# 3
-# 5
-# 7
-# 9
+student = {"name": "Mayuresh", "age": 23, "marks": 85}
+
+for key, value in student.items():
+    print(key+":",value)
+
+Output:
+name: Mayuresh
+age: 23
+marks: 85
+
+
+rows = 5
+for i in range(1, rows + 1):
+    for j in range(i):
+        print("*", end="")
+    print()
+
+Output:
+*
+**
+***
+****
+*****
 
 
 
-#pass statement
-# for num in range(1,5):
-#     if num == 3:
-#         pass    #do nothing when num is 3
-#     print(num)
-# Output:
-# 1
-# 2
-# 3
-# 4
+Break Statement
+for num in range(1,10):
+    if num == 5:
+        break  # breaking the current loop
+    print(num)
+
+Output:
+1
+2
+3
+4
+
+
+
+continue statement
+for num in range(1,10):
+    if num%2 == 0:
+        continue    #here continue statement skip the even numbers
+    print(num)
+
+Output:
+1
+3
+5
+7
+9
+
+
+
+pass statement
+for num in range(1,5):
+    if num == 3:
+        pass    #do nothing when num is 3
+    print(num)
+Output:
+1
+2
+3
+4
 
 
 
