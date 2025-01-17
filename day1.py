6f54803e212429a8b16ae6e37703fbb6e698e740
mayureshpawashe <mayureshpawashe3036@gmail.com>
2025-01-17 11:36:58 +0530

    haa

---
+++ day1.py
@@ -0,0 +1,71 @@
+# #type of variables
+# x=5
+# y=34.433
+# z=("mayur","hii")
+# a=["mayur","goodmorning"]
+# print(type(x))
+# print(type(y))
+# print(type(z))
+# print(type(a))
+# Output:
+# <class 'int'>
+# <class 'float'>
+# <class 'tuple'>
+# <class 'list'>
+
+# Simple arithmetic operations
+# a = 10
+# b = 5
+#
+# print("Addition:", a + b)       # Output: 15
+# print("Subtraction:", a - b)    # Output: 5
+# print("Multiplication:", a * b) # Output: 50
+# print("Division:", a / b)       # Output: 2.0
+
+# String concatenation
+# a = "Hello"
+# name = str(input("Enter Your Name: "))
+# print(a + ", " + name + "!")  # Output: Hello, Alice!
+#
+# output:
+# Enter Your Name: Mayuresh
+# Hello, Mayuresh!
+
+#Dictionary to store personal details
+# personal_details = {
+#     "name": "Mayuresh",
+#     "age": 23,
+#     "city": "Shrirampur",
+#     "pincode":413709,
+#     "hobbies": ["reading", "cycling", "Travelling"]
+#                     }
+
+#Accessing my personal info
+# print("Name is",personal_details["name"])
+# print("Age is",personal_details["age"])
+#
+# Output:
+# Name is Mayuresh
+# Age is 23
+
+
+#Typecasting
+
+#String to Tuple
+# y='mayuresh'
+# x=tuple(y)
+# print(type(x))
+# print(x)
+#
+# Output:
+# <class 'tuple'>
+# ('m', 'a', 'y', 'u', 'r', 'e', 's', 'h')
+
+
+
+#String to Integer
+
+
+
+
+
