fdebaceed796b5ae2aba7a360d07b4913e7642e7
mayureshpawashe <mayureshpawashe3036@gmail.com>
2025-01-22 16:49:17 +0530

    day5

---
+++ .ipynb_checkpoints/day5-checkpoint.py
@@ -0,0 +1,26 @@
+import pandas as pd
+
+# mydataset = {
+#   'cars': ["BMW", "Volvo", "Ford"],
+#   'passings': [3, 7, 2],
+#   'price':[200000,450000,100000]
+# }
+#
+# myvar = pd.DataFrame(mydataset)
+#
+# print(myvar)
+#
+#
+# Output:
+#     cars  passings   price
+# 0    BMW         3  200000
+# 1  Volvo         7  450000
+# 2   Ford         2  100000
+
+
+#series
+
+s= [1,12,13,15]
+var= pd.Series(s)
+print(var)
+
